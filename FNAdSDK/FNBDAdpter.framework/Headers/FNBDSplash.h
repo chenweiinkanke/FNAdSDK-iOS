ba90202c07738b51cd40a1c5095c3a59d994d0f3
HL <helei@cdcyi.cn>
2021-10-19 17:07:43 +0800

    1.修复了激励视屏奖励回调两次的问题 2.修改了百度错误回调对象

---
+++ FNAdSDK/FNBDAdpter.framework/Headers/FNBDSplash.h
@@ -0,0 +1,16 @@
+//
+//  FNBDSplash.h
+//  FNAD
+//
+//  Created by M967 on 2021/09/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FNBDSplash : NSObject
+- (void)register_notification:(id)obj params:(NSDictionary *)adParams;
+@end
+
+NS_ASSUME_NONNULL_END
