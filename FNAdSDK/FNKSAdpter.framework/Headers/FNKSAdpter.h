655dfae59787b01e28a95c73c20d55a816c1c5c7
HL <helei@cdcyi.cn>
2021-12-13 17:09:27 +0800

    更新GDT

---
+++ FNAdSDK/FNKSAdpter.framework/Headers/FNKSAdpter.h
@@ -0,0 +1,23 @@
+//
+//  FNKSAdpter.h
+//  FNKSAdpter
+//
+//  Created by ALAN on 2021/7/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FNKSAdpter.
+FOUNDATION_EXPORT double FNKSAdpterVersionNumber;
+
+//! Project version string for FNKSAdpter.
+FOUNDATION_EXPORT const unsigned char FNKSAdpterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FNKSAdpter/PublicHeader.h>
+
+// ********************* 当前版本: 3.3.18.1 *********************
+
+#import <FNKSAdpter/FNKSSDKAdpter.h>
+#import <FNKSAdpter/FNKSSplash.h>
+#import <FNKSAdpter/FNKSInterstitial.h>
+#import <FNKSAdpter/FNKSReardVideoAdpter.h>
